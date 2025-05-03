58dedaa746b90a5d18a5b1568ee297f87d239665
Nikita Boguslavskii <bognik3@gmail.com>
2025-05-03 16:32:19 -0400

    feat: Add PickAndPlace

--- PickAndPlace.m
+++ PickAndPlace.m
@@ -6,11 +6,8 @@ addpath('kinematics');
 addpath('kinematics/lib');
 
 global I; % Initialize the global variable I for integral state
-I = 0;
-
-
-% Create the environment
-g = [0 0 -9.81]; % Gravity Vector [m/s^2]
+I = zeros(3,1);
+simulationTime = 0;
 
 % Create the robot
 robot = make_robot();
@@ -23,11 +20,20 @@ n = size(S,2); % read the number of joints
 %% Control the motion of the robot between 2 set points
 fprintf('----------------------Kinematic Control of a 3-DoF Arm--------------------\n');
 
-path = [0 0.3 0.0;
-        0.25 0 0.1; ...
-        0.25 0.25 0.2]';
-
+path = [0.3 0.0 0.2; ...
+        0.0 0.3 0.2; ...
+        0 0.3 0.0; ...
+        0.0 0.3 0.2; ...
+        0.3 0.0 0.2; ...
+        0.0 -0.3 0.2; ...
+        0.0 -0.3 0.0; ...
+        0.0 -0.3 0.2; ...
+        0.3 0.0 0.2; ...
+        ]';
 nPts = size(path,2);
+Time = 5;
+% SimulationTines is an array of nPts-1 Time
+simulationTimes = [Time * ones(1, nPts-1)]; % Simulation time for each segment
 
 fprintf('Calculating the Inverse Kinematics... ');
 scatter3(path(1,:), path(2,:), path(3,:), 'filled');
@@ -90,24 +96,85 @@ for ii = 1 : nPts
     %% Controllers:
     X0 = [previousQ(1), previousQ(2), previousQ(3), 0, 0, 0];  % Initial State
     q_des = [currentQ(1), currentQ(2), currentQ(3)]; % Desired State
-    tf = 10;  % Simulation time
-    clear X  % Clear the variable 'X' to asvoid conflicts with the function
 
-    % PD Control:
-    [T, X] = ode45(@(t, x) planarArmODE_PDGravity(t, x, q_des), [0 tf], X0);
+    if ii > 1
+        tf = simulationTimes(ii-1);  % Simulation time
+        simulationTime = simulationTime + tf; % Update the total simulation time
+        clear X  % Clear the variable 'X' to asvoid conflicts with the function
 
-    trajectory = [trajectory; X]; % Store the trajectory
-    previousQ = currentQ; 
+        % Load on the end-effector [kg]
+        load = 0.25; 
+        controller = 'CTC'; % Choose the controller: 'PD', 'PID', or 'CTC'
+
+        %% PD Control:
+        if strcmp(controller, 'PD')
+            [T, X] = ode45(@(t, x) planarArmODE_PDGravity(t, x, q_des, load), [0 tf], X0);
+        end
+        
+
+        %% PID Control:
+        if strcmp(controller, 'PID')
+            [T, X] = ode45(@(t, x) planarArmODE_PIDGravity(t, x, q_des, load), [0 tf], X0);
+        end
+
+        %% CTC and Trajectory Generation:
+        if strcmp(controller, 'CTC')
+            % Inititalize the variables where we will store the torque profiles, joint
+            % positions, and time, so that we can display them later.
+            jointPos = [];
+            jointVel = [];
+            jointAcc = [];
+            t = [];
+
+            dt = 1e-3;
+            t_local = 0:dt:5;
+    
+            % Store segment trajectory
+            jointPos_prescribed = zeros(n, length(t_local));
+            jointVel_prescribed = zeros(n, length(t_local));
+            jointAcc_prescribed = zeros(n, length(t_local));
+    
+            for jj = 1 : n
+                params_traj.t = [0 t_local(end)];
+                params_traj.dt = dt;
+                params_traj.q = [waypoints(jj, ii-1), waypoints(jj, ii)];
+                params_traj.v = [0 0];
+                params_traj.a = [0 0];
+                
+                traj = make_trajectory('quintic', params_traj);
+                
+                jointPos_prescribed(jj,:) = traj.q;
+                jointVel_prescribed(jj,:) = traj.v;
+                jointAcc_prescribed(jj,:) = traj.a;
+            end
+
+            % Simulate the segment.
+            [T, X] = ode45(@(t, x) planarArmODE_CTCGravity(t, x, t_local, ...
+                jointPos_prescribed, jointVel_prescribed, jointAcc_prescribed, load), ...
+                [0 t_local(end)], X0);
+        end
+
+        trajectory = [trajectory; X]; % Store the trajectory
+        previousQ = X(end, 1:3); % Update the previousQ for the next iteration
+        I = zeros(3,1); % Reset the integral state for the next iteration
+    end
 end
 
 fprintf('\nDone. Simulating the robot...');
 
 %% Animate the robot
 title('Point2point Trajectory');
-% robot.plot(jointPos(:,1:100:end)','trail',{'r', 'LineWidth', 2});
 % Start a timer:
 tic
-robot.plot(trajectory(1:280:end, 1:3),'trail',{'r', 'LineWidth', 2});
+fps = 30;
+N = size(trajectory, 1);
+n_frames = fps * simulationTime * 0.478;
+
+% Downsample to fixed number of frames
+idx = round(linspace(1, N, n_frames));
+frames = trajectory(idx, 1:3);
+
+robot.plot(frames, 'fps', fps, 'trail', {'r', 'LineWidth', 2});
 % End the timer:
 toc
 % Show the duration:
